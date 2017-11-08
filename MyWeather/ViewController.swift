3cc33a9948af40f8e328617057d6a5890b639e8c
htakada-cmlab <htakada@cs.ritsumei.ac.jp>
2017-11-08 15:53:50 +0900

    Initial version

--- MyWeather/ViewController.swift
+++ MyWeather/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var cities: [String] = ["Kyoto", "Tokyo", "Sapporo", "Honolulu"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Weather", for: indexPath)
+        cell.textLabel?.text = cities[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=" + cities[indexPath.row] + "&appid=d01ed74f13d285ba9f785fb49335bf3a"
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                print(json)
+
+                let weather = json["weather"][0]["description"].stringValue
+                let temp = json["main"]["temp"].floatValue - 273.15
+                vc.detailItem = ["weather": weather, "temp": "\(temp)"]
+                navigationController?.pushViewController(vc, animated: true)
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
